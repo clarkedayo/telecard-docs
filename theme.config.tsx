1b7dbfdc8c70afb271c5ad62d1db9876aaf7dbbc
clarkedayo <144366993+clarkedayo@users.noreply.github.com>
2024-02-24 00:32:10 +0000

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>TELECARD</span>,
+  project: {
+    link: 'https://TELECARD.ORG',
+  },
+  docsRepositoryBase: 'https://github.com/',
+  footer: {
+    text: 'TeleCard Docs © 2024 | v1.0',
+  },
+}
+
+export default config
