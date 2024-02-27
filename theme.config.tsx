32acba15bdb4f995a01426f5c0d70efd1407ef1c
clarkedayo <144366993+clarkedayo@users.noreply.github.com>
2024-02-27 19:45:11 +0000

    Add files via upload

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>TELECARD Docs</span>,
+  logo: <span>TeleCard Docs</span>,
   project: {
     link: 'https://TELECARD.ORG',
-  }
+  },
+  head: <>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="TeleCard When privacy is important." />
+  <meta name="og:title" content="TeleCard allows you to spend your crypto completely Anonymous 🎭. No KYC. Instant Cards. Untraceable." />
+  </>
 }
 
 export default config
